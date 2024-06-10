cc59f0498b6c12a8953b7b6898df49658f5e1516
avikinkitsolutions <avik.saha0104@gmail.com>
2024-06-10 13:50:52 +0530

    Issue Resolved

--- srv/service.js
+++ srv/service.js
@@ -6,22 +6,14 @@ const { getJobApplicationInterview } = require("./src/jobApplicationInterview");
 const { getPositionsDetails } = require("./src/position"); // Import the handler
 
 module.exports = cds.service.impl(async (srv) => {
-  // Getting the entities from the services.
-  const {
-    User,
-    JobRequisition,
-    JobApplication,
-    jobApplicationInterview,
-    Position,
-  } = srv.entities;
-  srv.on("READ", User, getUsersDetails);
-  srv.on("READ", JobRequisition, getJobRequisition);
-  srv.on("READ", JobApplication, getJobApplication);
-  srv.on("READ", jobApplicationInterview, getJobApplicationInterview);
-  srv.on("READ", Position, getPositionsDetails);
+  srv.on("READ", "User", getUsersDetails);
+  srv.on("READ", "JobRequisition", getJobRequisition);
+  srv.on("READ", "JobApplication", getJobApplication);
+  srv.on("READ", "jobApplicationInterview", getJobApplicationInterview);
+  srv.on("READ", "Position", getPositionsDetails);
   srv.on(
     ["CREATE", "UPDATE"],
-    jobApplicationInterview,
+    "jobApplicationInterview",
     getJobApplicationInterview
   );
 });

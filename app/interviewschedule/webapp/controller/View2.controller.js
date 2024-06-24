846f969083bb4bae099aa4d195614e777a0fa6b7
soumikinkitsolution <soumik.s@inkitsolutions.com>
2024-06-24 19:39:38 +0530

    commit

--- app/interviewschedule/webapp/controller/View2.controller.js
+++ app/interviewschedule/webapp/controller/View2.controller.js
@@ -5,9 +5,11 @@ sap.ui.define(
     "sap/ui/core/date/UI5Date",
     "sap/ui/unified/library",
     "sap/ui/core/Fragment",
-    "sap/ui/core/format/DateFormat"
+    "sap/ui/core/format/DateFormat",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
   ],
-  function (BaseController, JSONModel, UI5Date, unifiedLibrary, Fragment, DateFormat) {
+  function (BaseController, JSONModel, UI5Date, unifiedLibrary, Fragment, DateFormat, Filter, FilterOperator) {
     "use strict";
 
     //Selecting the calendar type 
@@ -20,14 +22,27 @@ sap.ui.define(
 
         this.oRouter = this.getOwnerComponent().getRouter();
         this.oRouter.getRoute("RouteView2").attachMatched(this.loadPage, this);
-        this.initialSetup();
+
 
       },
 
       loadPage: async function (oEvent) {
-        
+
+        //Create Empty array
+        let aApplicationIds = []
+
         //Getting the ApplicationId 
         var AppId = oEvent.getParameter("arguments").variable
+
+        //Application Ids Array 
+        aApplicationIds = [...aApplicationIds, AppId]
+
+        // Job applications filter 
+        const aJobApplicationfilters = aApplicationIds.map(
+          (applicationId) =>
+            new Filter("applicationId", FilterOperator.EQ, applicationId)
+        );
+
         // oData model object
         const oDataModel = this.getOwnerComponent().getModel();
 
@@ -35,74 +50,38 @@ sap.ui.define(
         const { results: aData } = await this.getData(
           oDataModel,
           "/JobApplicationInterview",
-          AppId,
+          aJobApplicationfilters,
           [],
           this.iTop,
           this.iSkip
         );
-        console.log(aData)
 
-      },
-
-      //+++++++++++++++++++++++++++++++ Implementing the initial setup +++++++++++++++++++++++++++++++
-      initialSetup: function () {
-        // Create a Json Model
-        var oModel = new JSONModel();
-
-        // Set Data to the json model
-
-        oModel.setData({
-          appointments: [
-            {
-              title: "L1",
-              type: CalendarDayType.Type05,
-              startDate: UI5Date.getInstance("2024", "5", "23", "0", "0"),
-              endDate: UI5Date.getInstance("2024", "5", "23", "0", "0")
-            },
-            {
-              title: "L2",
-              type: CalendarDayType.Type05,
-              startDate: UI5Date.getInstance("2024", "5", "24", "5", "10"),
-              endDate: UI5Date.getInstance("2024", "5", "24", "6", "10")
-            }]
-
+        //Creating the Json model
+        var oModel = new JSONModel({
+          appointments: aData,
+          StartingDate: UI5Date.getInstance("2022", "0", "22"),
         });
 
-        //Set the Json Model to the View level
-        this.getView().setModel(oModel);
 
-        //Creating the json model object and Name of the model is allDay
-        oModel = new JSONModel();
-        oModel.setData({ allDay: false });
-        this.getView().setModel(oModel, "allDay");
+        //Set the Json Model to the View level
+        this.getView().setModel(oModel, "local");
 
       },
 
       handleAppointmentSelect: function (oEvent) {
-        debugger
 
-        //Declaring the variables
 
         var oAppointment = oEvent.getParameter("appointment"),
-          oStartDate,
-          oEndDate,
-          oTrimmedStartDate,
-          oTrimmedEndDate,
-          bAllDate,
-          oModel,
           oView = this.getView();
+        const AppoinmentData = oEvent.getParameter("appointment").getBindingContext("local").getObject()
+        this.getView().getModel("local").setProperty("/currentAppoinments", AppoinmentData);
 
 
         if (oAppointment === undefined) {
           return;
         }
+        // bAllDate = false;
 
-        oStartDate = oAppointment.getStartDate();
-        oEndDate = oAppointment.getEndDate();
-        oTrimmedStartDate = UI5Date.getInstance(oStartDate);
-        oTrimmedEndDate = UI5Date.getInstance(oEndDate);
-        bAllDate = false;
-        oModel = this.getView().getModel("allDay");
 
 
         if (!oAppointment.getSelected() && this.AppoinmentPopup) {
@@ -112,19 +91,7 @@ sap.ui.define(
           return;
         }
 
-        this.setHoursToZero(oTrimmedStartDate);
-        this.setHoursToZero(oTrimmedEndDate);
 
-        //This condition is basically check whether the Appoinment 
-        // is for Whole day or particular time
-        if (oStartDate.getTime() === oTrimmedStartDate.getTime() && oEndDate.getTime() === oTrimmedEndDate.getTime()) {
-          bAllDate = true;
-        }
-
-        //Adding the bAllDate value to json model
-        oModel.getData().allDay = bAllDate;
-        oModel.updateBindings();
-        //Opening the Popup when we click on the Appoinment
         if (!this.AppoinmentPopup) {
           this.AppoinmentPopup = Fragment.load({
             id: oView.getId(),
@@ -136,32 +103,29 @@ sap.ui.define(
           });
         }
         this.AppoinmentPopup.then(function (oFragment) {
-          oFragment.setBindingContext(oAppointment.getBindingContext());
+          oFragment.setBindingContext(oAppointment.getBindingContext("local"));
           oFragment.openBy(oAppointment);
         });
 
       },
-      //This function covert the hours part to zero.
-      setHoursToZero: function (oDate) {
-        oDate.setHours(0, 0, 0, 0);
-      },
+
 
       formatDate: function (oDate) {
         if (oDate) {
-          var iHours = oDate.getHours(),
-            iMinutes = oDate.getMinutes(),
-            iSeconds = oDate.getSeconds();
-
-          if (iHours !== 0 || iMinutes !== 0 || iSeconds !== 0) {
-            return DateFormat.getDateTimeInstance({ style: "medium" }).format(oDate);
-          } else {
-            return DateFormat.getDateInstance({ style: "medium" }).format(oDate);
-          }
+            var iHours = oDate.getHours(),
+                iMinutes = oDate.getMinutes(),
+                iSeconds = oDate.getSeconds();
+    
+            // Define a custom date format with desired pattern
+            var oDateFormat = sap.ui.core.format.DateFormat.getDateTimeInstance({
+                pattern: "dd MMM yyyy, HH:mm:ss"
+            });
+    
+            return oDateFormat.format(oDate);
         }
-      },
-
-
-        // Get Data function implementation
+    },
+    
+      // Get Data function implementation
       getData: async function (
         oDataModel,
         sPath,

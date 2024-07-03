1e35a0af965fad18dfbaa3d095cedb6fd9a75eb6
soumikrepo <soumiksahacodedevpro@gmail.com>
2024-07-03 22:57:08 +0530

    commit

--- app/interviewschedule/webapp/controller/View2.controller.js
+++ app/interviewschedule/webapp/controller/View2.controller.js
@@ -28,246 +28,114 @@ sap.ui.define(
     "use strict";
 
     return BaseController.extend("com.app.interviewschedule.controller.View2", {
-      //Setting the formatter
-      formatter: formatter,
+
 
       // +++++++++++++++++++++++++++++++ Defined the onInit function +++++++++++++++++++++++++++++++++++++
       onInit: function () {
+
         this.oRouter = this.getOwnerComponent().getRouter();
+        this._initialSetup();
         this.oRouter.getRoute("RouteView2").attachMatched(this.loadPage, this);
 
         //Get the Date Object
         this.currentDate = new Date();
       },
 
+      _initialSetup: function () {
+        this.iTop = 10
+        this.iSkip = 0
+
+        //set the json model view level
+        this.getView().setModel(
+          new JSONModel({
+
+            Interviewer: [],
+            selectedInterviewer:[]
+          }), "local"
+        );
+      },
+
       loadPage: async function (oEvent) {
 
-        debugger
-        //Create Empty array
-        let aApplicationIds = [];
+        //model object
+        const oModel = this.getOwnerComponent().getModel();
 
-        //Getting the ApplicationId
-        this.AppId = oEvent.getParameter("arguments").variable;
-
-        //Application Ids Array
-        aApplicationIds = [...aApplicationIds, this.AppId];
-
-        // Job applications filter
-        const aJobApplicationfilters = aApplicationIds.map(
-          (applicationId) =>
-            new Filter("applicationId", FilterOperator.EQ, applicationId)
-        );
-
-        // oData model object
-        const oDataModel = this.getOwnerComponent().getModel();
-
-        // Fetching the Particular Job Interview.
+        //Getting the all interviewer
         const { results: aData } = await this.getData(
-          oDataModel,
-          "/JobApplicationInterview",
-          aJobApplicationfilters,
+          oModel,
+          "/User",
+          [],
           [],
           this.iTop,
           this.iSkip
         );
 
-        //Creating the Json model
-        var oModel = new JSONModel({
-          appointments: aData,
+        //Assign the interviewer data to the local model
+        this.getView().getModel("local").setProperty("/Interviewer", aData)
+        this.getView().getModel("local").refresh(true);
 
-          StartingDate: UI5Date.getInstance(this.currentDate.getFullYear(), this.currentDate.getMonth(), this.currentDate.getDate()),
-          allDay: false,
-
-          // This Object we are using for creating the new Appoinment Popup
-          AppoinmentDetails: {
-            start_date: formatter.StartDateHardCodedTime(
-              this.getView().byId("SPC1").getStartDate()
-            ),
-            end_date: formatter.EndDateHardCodedTime(
-              this.getView().byId("SPC1").getStartDate()
-            ),
-            app_id: this.AppId,
-          },
-          enableAppointmentsDragAndDrop: true,
-          enableAppointmentsResize: true,
-        });
-
-        //Set the Json Model to the View level
-        this.getView().setModel(oModel, "local");
       },
 
-      //++++++++++This event will be trigger when we will click on any event+++++++++++++++
+      oaddNewInterviewerPopup: null,
+      addNewInterviewer: function (oEvent) {
 
-      handleAppointmentSelect: function (oEvent) {
-        var oAppointment = oEvent.getParameter("appointment"),
-          oView = this.getView();
-        const AppoinmentData = oEvent
-          .getParameter("appointment")
-          .getBindingContext("local")
-          .getObject();
-        this.getView()
-          .getModel("local")
-          .setProperty("/currentAppoinments", AppoinmentData);
+         //  Fragment to be loaded and Opened
+         var that = this;
+         if (!this.oaddNewInterviewerPopup) {
+           Fragment.load({
+             name: "com.app.interviewschedule.fragments.selectInterviewer",
+             controller: this,
+             id: this.getView().getId(),
+           }).then(function (oFragment) {
+             that.oaddNewInterviewerPopup = oFragment;
+             that.getView().addDependent(that.oaddNewInterviewerPopup);
+             that.oaddNewInterviewerPopup.open();
+           });
+         } else {
+           this.oaddNewInterviewerPopup.open();
+         }
 
-        if (oAppointment === undefined) {
-          return;
-        }
-
-        if (!oAppointment.getSelected() && this.AppoinmentPopup) {
-          this.AppoinmentPopup.then(function (oFragment) {
-            oFragment.close();
-          });
-          return;
-        }
-
-        if (!this.AppoinmentPopup) {
-          this.AppoinmentPopup = Fragment.load({
-            id: oView.getId(),
-            name: "com.app.interviewschedule.fragments.Details",
-            controller: this,
-          }).then(function (oFragment) {
-            oView.addDependent(oFragment);
-            return oFragment;
-          });
-        }
-        this.AppoinmentPopup.then(function (oFragment) {
-          oFragment.setBindingContext(oAppointment.getBindingContext("local"));
-          oFragment.openBy(oAppointment);
-        });
       },
 
-      //++++++++++++++++++++++Create appoinment dialog+++++++++++++++++++++++++++++++++++
+      onDialogSelect : function(oEvent)
+      {
 
-      oCreateAppoinmentPopup: null,
-      handleAppointmentCreate: function (oEvent) {
-        this.AppId;
-        var that = this;
+          //get all the selected item
+          var oSeletecItems = oEvent.getParameter("selectedItems")
 
-        if (!this.oCreateAppoinmentPopup) {
-          Fragment.load({
-            name: "com.app.interviewschedule.fragments.CreateAppointment",
-            controller: this,
-            id: that.getView().getId(),
-          }).then(function (oFragment) {
-            that.oCreateAppoinmentPopup = oFragment;
-            that.getView().addDependent(that.oCreateAppoinmentPopup);
-            that.oCreateAppoinmentPopup.setTitle("Create new appoinment");
-            that.oCreateAppoinmentPopup.open();
-          });
-        } else {
-          this.oCreateAppoinmentPopup.open();
-        }
+          //loop through the selected items
+          oSeletecItems.forEach((item)=>{
+
+          // Get the binding context for each selected item
+          var oBindingContext = item.getBindingContext("local");
+
+           // Retrieve the data object bound to the selected item
+           var oDataObject = oBindingContext.getObject();
+
+           this.getView().getModel("local").getData().selectedInterviewer.push(oDataObject)
+          
+          })
+
+          // Refresh the model to reflect the changes
+          this.getView().getModel("local").refresh(true);
+
+          //Clear the selection in the dialog
+          oSeletecItems.forEach((item)=>{
+            item.setSelected(false)
+          })
+
+        
       },
 
-      // +++++++++++++ Here we are Drop the Appoinment after Drag +++++++++++++++++++++
-
-      handleAppointmentDrop: function (oEvent) {
-        var oAppointment = oEvent.getParameter("appointment");
-        var oStartDate = oEvent.getParameter("startDate");
-        var oEndDate = oEvent.getParameter("endDate");
-        var bCopy = oEvent.getParameter("copy");
-        var sAppointmentTitle = oAppointment.getTitle();
-        var oModel = this.getView().getModel("local");
-        var oNewAppoinment;
-
-        if (bCopy) {
-          oNewAppoinment = {
-            title: sAppointmentTitle,
-            icon: oAppointment.getIcon(),
-            // text: oAppointment.getText(),
-            type: oAppointment.getType(),
-            startDate: oStartDate,
-            endDate: oEndDate,
-          };
-          oModel.getData().appointments.push(oNewAppoinment);
-          oModel.updateBindings();
-        } else {
-          oAppointment.setStartDate(oStartDate);
-          oAppointment.setEndDate(oEndDate);
-        }
-
-        MessageToast.show(
-          "Appointment with title \n'" +
-            sAppointmentTitle +
-            "'\n has been " +
-            (bCopy ? "create" : "moved")
-        );
+      onDialogClose : function()
+      {
+        alert("Bye")
       },
 
-      //+++++++++++++++++++++ Here we resize the Appoinment ++++++++++++++++++++++++
 
-      handleAppointmentResize: function (oEvent) {
-        var oAppointment = oEvent.getParameter("appointment"),
-          oStartDate = oEvent.getParameter("startDate"),
-          oEndDate = oEvent.getParameter("endDate"),
-          sAppointmentTitle = oAppointment.getTitle();
-
-        oAppointment.setStartDate(oStartDate);
-        oAppointment.setEndDate(oEndDate);
-
-        MessageToast.show(
-          "Appointment with title \n'" +
-            sAppointmentTitle +
-            "'\n has been resized"
-        );
-      },
-      // ++++++++++++++++ Close Popup +++++++++++++++++++++
-      closeAppoinment: function (oEvent) {
-        this.getView().getModel("local").setProperty("/allDay", false);
-        this.getView()
-          .byId("DTP1")
-          .setValue(
-            formatter.StartDateHardCodedTime(
-              this.getView().byId("SPC1").getStartDate()
-            )
-          );
-        this.getView()
-          .byId("DTP2")
-          .setValue(
-            formatter.EndDateHardCodedTime(
-              this.getView().byId("SPC1").getStartDate()
-            )
-          );
-        oEvent.getSource().getParent().getParent().close();
-      },
-
-      // +++++++++++++++++++++++ When we click the AllDay checkbox +++++++++++++++++
-      handleCheckBoxSelect: function (oEvent) {
-        let AllDay = oEvent.getSource().getSelected(),
-          StartDate = this.getView()
-            .getModel("local")
-            .getProperty("/AppoinmentDetails").start_date,
-          EndDate = this.getView()
-            .getModel("local")
-            .getProperty("/AppoinmentDetails").end_date;
-
-        if (AllDay) {
-          let ModifiedStartDate = formatter.removeTimePart(StartDate);
-          let ModifiedEndDate = formatter.removeTimePart(EndDate);
-
-          this.getView().byId("DTP1").setValue(ModifiedStartDate);
-          this.getView().byId("DTP2").setValue(ModifiedEndDate);
-        } else {
-          this.getView()
-            .byId("DTP1")
-            .setValue(
-              formatter.StartDateHardCodedTime(
-                this.getView().byId("SPC1").getStartDate()
-              )
-            );
-          this.getView()
-            .byId("DTP2")
-            .setValue(
-              formatter.EndDateHardCodedTime(
-                this.getView().byId("SPC1").getStartDate()
-              )
-            );
-        }
-      },
-
-      // ++++++++++++++++++ getData function implementation ++++++++++++++++++++++++++++
+      // Get Data function implementation
       getData: async function (
-        oDataModel,
+        oModel,
         sPath,
         aFilters = [],
         aExpand = [],
@@ -275,22 +143,20 @@ sap.ui.define(
         skip
       ) {
         sap.ui.core.BusyIndicator.show();
-        this.oDataModel = oDataModel;
+        this.oModel = oModel;
         this.sPath = sPath;
         this.aFilters = aFilters;
         this.aExpand = aExpand;
         top ? (this.top = top) : "";
         skip ? (this.skip = skip) : "";
 
-        const oUrlParameters = {
-          $expand: aExpand,
-        };
-
+        const oUrlParameters = {};
+        aExpand.length > 0 ? (oUrlParameters.$expand = aExpand) : "";
         top ? (oUrlParameters.$top = top) : "";
         skip ? (oUrlParameters.$skip = skip) : "";
 
         return new Promise((resolve, reject) => {
-          oDataModel.read(sPath, {
+          oModel.read(sPath, {
             urlParameters: oUrlParameters,
             filters: [...aFilters],
             success: function (oSuccessData) {
@@ -304,6 +170,7 @@ sap.ui.define(
           });
         });
       },
+
     });
   }
 );
